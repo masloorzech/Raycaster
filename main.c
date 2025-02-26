4f9c40f618f65ce71f4829d26b2efc03321f83e9
AntoniIwan <245819@edu.p.lodz.pl>
2025-02-26 17:37:12 +0100

    Addad player movement and fixed some map problems

--- main.c
+++ main.c
@@ -3,22 +3,26 @@
 #include <stdbool.h>
 #include <math.h>
 
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
+#define SCALE_FACTOR 1.5
+
+#define SCREEN_WIDTH (640 * SCALE_FACTOR)
+#define SCREEN_HEIGHT (480 * SCALE_FACTOR)
 #define WINDOW_NAME "SIMPLE RAYCATER GAME"
+
+
 
 #define MAP_WIDTH 9
 #define MAP_HEIGHT 9
 
 uint8_t map[MAP_HEIGHT][MAP_WIDTH] = {
     {1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 2, 0, 3, 0, 0, 1},
-    {1, 0, 1, 1, 0, 1, 1, 0, 1},
+    {1, 0, 0, 0, 1, 0, 0, 0, 1},
+    {1, 0, 0, 0, 1, 0, 0, 0, 1},
+    {1, 0, 0, 0, 1, 0, 0, 0, 1},
     {1, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 4, 1, 0, 0, 0, 1, 4, 1},
+    {1, 0, 0, 0, 0, 1, 1, 1, 1},
     {1, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 1, 1, 0, 1, 1, 0, 1},
-    {1, 0, 0, 2, 0, 3, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 1},
     {1, 1, 1, 1, 1, 1, 1, 1, 1}
 };
 
@@ -27,6 +31,8 @@ int main(int argc, char *argv[]) {
         printf("SDL_Init Error: %s\n", SDL_GetError());
         return -1;
     }
+    SDL_SetRelativeMouseMode(SDL_TRUE);
+
     SDL_Window* main_window = SDL_CreateWindow(WINDOW_NAME, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (main_window == NULL) {
         printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
@@ -47,6 +53,7 @@ int main(int argc, char *argv[]) {
     //Move vector
     double dirX = -1.0;
     double dirY = 0.0;
+
     //Camera
     double planeX =0;
     double planeY = 0.66; //Camera plane
@@ -56,12 +63,12 @@ int main(int argc, char *argv[]) {
 
     double h = (double)SCREEN_HEIGHT;
 
+    double playerAngle = -M_PI / 2;  // Początkowy kąt - patrzymy na lewo (90° w lewo od osi X)
+
+    bool moveForward = false, moveBackward = false;
     while (run) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                run = false;
-            }
-        }
+
+
         SDL_RenderClear(renderer);
         // Wypełnienie sufitu (górna połowa ekranu)
         SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255); // Szary sufit
@@ -82,17 +89,18 @@ int main(int argc, char *argv[]) {
             int mapX = (int)playerX;
             int mapY = (int)playerY;
 
-            double sideDistX;
-            double sideDistY;
+            double sideDistX =0;
+            double sideDistY =0;
 
             double deltaDistX = (rayDirX == 0) ? 1e30 : fabs(1.0 / rayDirX);
             double deltaDistY = (rayDirY == 0) ? 1e30 : fabs(1.0 / rayDirY);
 
-            double perpWallDist;
+            double perpWallDist=0;
             int stepX = 0;
             int stepY =0;
-            int hit = 0;
-            int side;
+            int hit =0;
+            int side=0;
+
             if (rayDirX < 0) {
                 stepX -= 1;
                 sideDistX = (playerX - mapX) * deltaDistX;
@@ -120,7 +128,7 @@ int main(int argc, char *argv[]) {
                     mapY += stepY;
                     side = 1;
                 }
-                if (map[mapY][mapX] > 0) {
+                if (map[mapX][mapY] > 0) {
                     hit = 1;
                 }
             }
@@ -161,6 +169,68 @@ int main(int argc, char *argv[]) {
         }
         SDL_RenderPresent(renderer);
 
+        oldTime = time;
+        time = SDL_GetTicks();
+        double frameTime = (time - oldTime) / 1000.0;
+
+        double moveSpeed = frameTime * 5.0;
+
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                run = false;
+            }
+
+            if (event.type == SDL_KEYDOWN) {
+                if (event.key.keysym.sym == SDLK_ESCAPE) run = false;
+                if (event.key.keysym.sym == SDLK_w) moveForward = true;
+                if (event.key.keysym.sym == SDLK_s) moveBackward = true;
+            }
+            if (event.type == SDL_KEYUP) {
+                if (event.key.keysym.sym == SDLK_w) moveForward = false;
+                if (event.key.keysym.sym == SDLK_s) moveBackward = false;
+            }
+
+            int mouseX, mouseY;
+            SDL_GetRelativeMouseState(&mouseX, &mouseY);
+
+            // Zmienna kontrolująca czułość obrotu
+            double mouseSensitivity = 0.002;  // Zmieniaj tę wartość, aby dostosować czułość obrotu
+
+            // Prędkość rotacji na podstawie zmiany pozycji myszy w poziomie
+            playerAngle += mouseX * mouseSensitivity;
+
+            // Zabezpieczenie przed przekroczeniem granic
+            if (playerAngle < -M_PI) playerAngle += 2 * M_PI;
+            if (playerAngle > M_PI) playerAngle -= 2 * M_PI;
+
+            // Zaktualizuj kierunek
+            dirX = cos(playerAngle);
+            dirY = sin(playerAngle);
+
+            // Zaktualizowanie kamery (plane)
+            planeX = cos(playerAngle + M_PI / 2) * 0.66;
+            planeY = sin(playerAngle + M_PI / 2) * 0.66;
+        }
+
+        if (moveForward) {
+            if (map[(int)(playerX + dirX * moveSpeed)][(int)playerY] == 0) {
+                playerX += dirX * moveSpeed;
+            }
+
+            if (map[(int)playerX][(int)(playerY + dirY * moveSpeed)] == 0) {
+                playerY += dirY * moveSpeed;
+            }
+
+        }
+
+        if (moveBackward) {
+            if (map[(int)(playerX - dirX * moveSpeed)][(int)playerY] == 0) {
+                playerX -= dirX * moveSpeed;
+            }
+            if (map[(int)playerX][(int)(playerY - dirY * moveSpeed)] == 0) {
+                playerY -= dirY * moveSpeed;
+            }
+        }
     }
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(main_window);
